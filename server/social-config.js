ed1893f2300693625db269747a6e9bef81ae116f
leoharsha <leo.harsha10@gmail.com>
2016-04-19 01:46:52 +0530

    first commit

---
+++ server/social-config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '991419040917507',
+    secret: '6fc5146e241cd411373f466ef163ddc1'
+});
